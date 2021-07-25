4ab2ee793d8f2428e2e3ba94c6086e085fa928c5
Bert W Summers <summersb@moesol.com>
2021-07-24 22:12:00 -0700

    Update config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,24 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  parser: '@typescript-eslint/parser',
-  plugins: [
-    '@typescript-eslint',
-  ],
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-  ],
-  rules: {
-    'semi': ['error', 'never'],
-    'no-console': "warn",
-    'quotes': ['error', 'single'],
-    'curly': "error",
-    'brace-style': ['error', 'stroustrup'],
-    'indent': ['error', "tab"],
-    'eol-last': ['error', 'always'],
-    '@typescript-eslint/no-unused-vars': "error"
-  }
-};
+	root: true,
+	env: {
+		node: true
+	},
+	parser: '@typescript-eslint/parser',
+	plugins: [
+		'@typescript-eslint',
+	],
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+	],
+	rules: {
+		'semi': ['error', 'never'],
+		'no-console': 'warn',
+		'quotes': ['error', 'single'],
+		'curly': 'error',
+		'brace-style': ['error', 'stroustrup'],
+		'indent': ['error', 'tab'],
+		'eol-last': ['error', 'always'],
+		'@typescript-eslint/no-unused-vars': 'error'
+	}
+}
